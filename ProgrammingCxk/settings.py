ce0b97bd80140c789c055742fc2c455ffacc7ee2
魏元昊 <vayyenhow@outlook.com>
2020-06-21 14:10:58 +0800

    更新部署设置

--- ProgrammingCxk/settings.py
+++ ProgrammingCxk/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ProgrammingCxk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
--- ProgrammingCxk/wsgi.py
+++ ProgrammingCxk/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+from os.path import dirname, abspath
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProgrammingCxk.settings')
+PROJECT_DIR = dirname(dirname(abspath(__file__)))
+sys.path.insert(0, PROJECT_DIR)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "ProgrammingCxk.settings"
 
 application = get_wsgi_application()
